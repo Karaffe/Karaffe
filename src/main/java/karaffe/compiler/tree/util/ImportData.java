8d8d3c3543b5555dae7cf002d8a44c8a4c2c7cd4
noko <nokok.kz@gmail.com>
2015-04-27 00:14:51 +0900

    ImportDataクラスの追加

---
+++ src/main/java/karaffe/compiler/tree/util/ImportData.java
@@ -0,0 +1,32 @@
+/**
+ * Karaffe Programming Language
+ */
+package karaffe.compiler.tree.util;
+
+import karaffe.compiler.tree.name.PackageOrTypeName;
+
+public class ImportData {
+
+    private final String shortName;
+    private final String fullName;
+
+    public ImportData(String shortName, String fullName) {
+        this.shortName = shortName;
+        this.fullName = fullName;
+    }
+
+    public ImportData(PackageOrTypeName packageOrTypeName) {
+        String path = packageOrTypeName.toPath();
+        this.fullName = path;
+        String[] split = path.split(".");
+        this.shortName = split[split.length - 1];
+    }
+
+    public String shortName() {
+        return shortName;
+    }
+
+    public String fullName() {
+        return fullName;
+    }
+}
