406b875ec576335fc6cfd673a8ea10b0baf89efc
noko <nokok.kz@gmail.com>
2015-04-26 16:22:18 +0900

    引数が無かった時にバージョン情報を表示するように修正

--- src/main/java/karaffe/Main.java
+++ src/main/java/karaffe/Main.java
@@ -7,6 +7,12 @@ import karaffe.compiler.KCompiler;
 public class Main {
 
     public static void main(String... args) throws Exception {
+        if (args == null || args.length == 0) {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.append("Karaffe Compiler: ").append(KCompiler.VERSION).append(System.lineSeparator());
+            stringBuilder.append("Java Version: ").append(System.getProperty("java.version")).append(System.lineSeparator());
+            System.out.println(stringBuilder.toString());
+        }
         Stream.of(args)
                 .map(File::new)
                 .map(KCompiler::new)
