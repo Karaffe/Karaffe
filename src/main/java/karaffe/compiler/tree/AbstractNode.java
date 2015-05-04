4b1893c3570725f052e35d368e7dedfe5e6c30e9
noko <nokok.kz@gmail.com>
2015-05-04 22:51:46 +0900

    AbstractNodeクラスにSymbol系のメソッドを追加

--- src/main/java/karaffe/compiler/tree/AbstractNode.java
+++ src/main/java/karaffe/compiler/tree/AbstractNode.java
@@ -7,10 +7,13 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
+import java_cup.runtime.Symbol;
 
 public abstract class AbstractNode implements AST {
 
     private final List<AST> children = new ArrayList<>();
+
+    private Optional<Symbol> symbol = Optional.empty();
 
     @Override
     public List<AST> children() {
@@ -25,4 +28,16 @@ public abstract class AbstractNode implements AST {
         Objects.requireNonNull(ast).ifPresent(children::add);
     }
 
+    public AbstractNode setSymbol(Object symbol) {
+        this.symbol = Optional.of(Symbol.class.cast(symbol));
+        return this;
+    }
+
+    public int line() {
+        return symbol.map(s -> s.left).orElse(-1);
+    }
+
+    public int column() {
+        return symbol.map(s -> s.right).orElse(-1);
+    }
 }
