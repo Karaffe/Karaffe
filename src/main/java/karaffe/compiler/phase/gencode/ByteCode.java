ec530219de7ad17addd4f75885a19de313a0e2dc
noko <nokok.kz@gmail.com>
2015-05-16 16:10:24 +0900

    クラスの命名規則に関するコードを削除

--- src/main/java/karaffe/compiler/phase/gencode/ByteCode.java
+++ src/main/java/karaffe/compiler/phase/gencode/ByteCode.java
@@ -3,9 +3,6 @@
  */
 package karaffe.compiler.phase.gencode;
 
-import karaffe.compiler.KCompiler;
-import karaffe.compiler.phase.ToDo;
-
 public class ByteCode {
 
     private final byte[] bytecode;
@@ -15,15 +12,6 @@ public class ByteCode {
     public ByteCode(byte[] bytecode, String fileName, String packagePrefix) {
         this.bytecode = bytecode;
         this.fileName = fileName;
-        if (fileName != null
-                && (Character.isLowerCase(fileName.charAt(0))
-                || fileName.charAt(0) == '_')) {
-            //クラス名の開始が小文字または_で始まる
-            KCompiler.todoList.add(
-                    new ToDo(ToDo.Type.WARNING, "クラス名はアルファベットの大文字(A~Z)から始める必要があります。\n"
-                            + "クラス名: " + fileName + "\n")
-            );
-        }
         this.packagePrefix = packagePrefix;
     }
 
