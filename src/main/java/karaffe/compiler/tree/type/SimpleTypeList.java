b4cd4a04caa6892c5897df13aa61737c17db6ff3
noko <nokok.kz@gmail.com>
2015-05-24 23:07:59 +0900

    SimpleTypeListの修正

--- src/main/java/karaffe/compiler/tree/type/SimpleTypeList.java
+++ src/main/java/karaffe/compiler/tree/type/SimpleTypeList.java
@@ -3,19 +3,23 @@
  */
 package karaffe.compiler.tree.type;
 
+import java.util.Iterator;
 import java.util.Optional;
-import karaffe.compiler.tree.AST;
 import karaffe.compiler.tree.AbstractNode;
 import karaffe.compiler.visitor.Visitor;
 
-public class SimpleTypeList extends AbstractNode {
+public class SimpleTypeList extends AbstractNode implements Iterator<SimpleTypeList> {
 
-    private final AST type;
-    private final Optional<AST> typeList;
+    private final SimpleType type;
+    private final Optional<SimpleTypeList> typeList;
+
+    public SimpleTypeList(Object t) {
+        this(t, null);
+    }
 
     public SimpleTypeList(Object t, Object l) {
-        this.type = (AST) t;
-        this.typeList = Optional.ofNullable((AST) l);
+        this.type = (SimpleType) t;
+        this.typeList = Optional.ofNullable((SimpleTypeList) l);
         addChild(type);
         addChild(typeList);
     }
@@ -23,6 +27,20 @@ public class SimpleTypeList extends AbstractNode {
     @Override
     public void accept(Visitor visitor) {
         visitor.simpleTypeList(this);
+    }
+
+    public SimpleType first() {
+        return type;
+    }
+
+    @Override
+    public SimpleTypeList next() {
+        return typeList.get();
+    }
+
+    @Override
+    public boolean hasNext() {
+        return typeList.isPresent();
     }
 
 }
